c39a41b295f37d6687c7ba7a156fe9d69a2f3c39
kevin <kevin@10.153.119.235>
2018-12-19 01:38:44 -0700

    Day 4 part 2, Day 5 part 1

---
+++ Day-5/Day5.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day5 {
+		
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner fileIn = new Scanner(new File("input.txt"));
+		String polymer1 = fileIn.next();
+		StringBuilder polymer = new StringBuilder(polymer1);
+		boolean cont = true;
+		while(cont) {
+			cont = false;
+			for(int i = 0; i < polymer.length() - 1 && !cont; i ++) {
+				if(Character.toLowerCase(polymer.charAt(i)) == Character.toLowerCase(polymer.charAt(i + 1)) 
+						&& ((Character.isUpperCase(polymer.charAt(i)) && Character.isLowerCase(polymer.charAt(i + 1)))
+					|| (Character.isLowerCase(polymer.charAt(i)) && Character.isUpperCase(polymer.charAt(i + 1))))) {
+					polymer.deleteCharAt(i);
+					polymer.deleteCharAt(i);
+					cont = true;
+				}
+			}
+
+		}	
+		System.out.println(polymer.length());
+	}
+	
+}
