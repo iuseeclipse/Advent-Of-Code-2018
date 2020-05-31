1735e2072ea99a751f9dd2334f23c36772180887
Kevin Liang <kev.liang@utexas.edu>
2020-05-30 22:27:12 -0500

    Day 8 part 2 complete

--- Day-8/Day_8.java
+++ Day-8/Day_8.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Day_8 {
 	
@@ -19,7 +22,7 @@ public class Day_8 {
 		
 		// populate list of nodes with recursive helper
 		List<Node> nodeList = new ArrayList<>();
-		helper(nodeList, dataList);
+		buildNodes(nodeList, dataList);
 		
 		/* part 1:----------------------------------------------------------- */
 		
@@ -31,27 +34,57 @@ public class Day_8 {
 		
 		/* part 2:----------------------------------------------------------- */
 		
-//		System.out.printf("Part 2 solution: %d\n", totalMetadata);
+		// root node is first to enter recursive stack, so last on the list
+		int rootValue = valueOfNode(nodeList.get(nodeList.size() - 1));
+		System.out.printf("Part 2 solution: %d\n", rootValue);
 	}
 	
 	/*
 	 * Recursive helper to build list of nodes
 	 */
-	public static void helper(List<Node> list, LinkedList<Integer> data) {
+	public static void buildNodes(List<Node> list, LinkedList<Integer> data) {
 		if (!data.isEmpty()) {
 			// determine number of children and metadata
 			int numChildren = data.removeFirst();
 			int numEntries = data.removeFirst();
 			// call recursive helper on children
+			Node[] children = new Node[numChildren];
 			for (int i = 0; i < numChildren; i++) {
-				helper(list, data);
+				buildNodes(list, data);
+				// new node added to end of list in memory
+				children[i] = list.get(list.size() - 1);
 			}
 			// determine metadata entries
 			int[] metadata = new int[numEntries];
 			for (int i = 0; i < numEntries; i++) {
 				metadata[i] = data.removeFirst();
-			}	
-			list.add(new Node(numChildren, metadata));
+			}
+			list.add(new Node(children, metadata));
+		}
+	}
+	
+	/*
+	 * Recursive helper to calculate value of a node, from its metadata and
+	 * its relative children in index order
+	 */
+	public static int valueOfNode(Node node) {
+		Node[] children = node.getChildren();
+		// if node has no children, value given by summation of metadata
+		if (children.length == 0) {
+			return node.getMetadataSum();
+		}
+		// otherwise, add the value of child nodes indexed by metadata
+		else {
+			int totalValue = 0;
+			for (int data : node.getMetadata()) {
+				// bounds check for index of child
+				if (data <= children.length) {
+					// value of nth child, subtract 1 for index in array
+					totalValue += valueOfNode(children[data - 1]);
+				}
+			}
+			
+			return totalValue;
 		}
 	}
 	
@@ -61,23 +94,23 @@ public class Day_8 {
 	static class Node {
 		
 		/*
-		 * header values
-		 */
-		private int numChildren;
-		private int numEntries;
-		
-		/*
 		 * metadata entries
 		 */
 		private int[] metadata;
 		
-		public Node (int numChildren, int[] metadata) {
-			this.numChildren = numChildren;
-			this.numEntries = metadata.length;
+		/*
+		 * node's children, in index order
+		 */
+		private Node[] children;
+		
+		public Node (Node[] children, int[] metadata) {
+			this.children = children;
 			this.metadata = metadata;
 		}
 		
-		// calculate and return the sum of the node's metadata, for part 1
+		/*
+		 * calculate and return the sum of the node's metadata, for part 1
+		 */
 		public int getMetadataSum() {
 			int total = 0;
 			for (int value : metadata) {
@@ -86,6 +119,19 @@ public class Day_8 {
 			return total;
 		}
 		
+		/*
+		 * return the node's metadata
+		 */
+		public int[] getMetadata() {
+			return metadata;
+		}
+		
+		/*
+		 * return the node's children, in index order
+		 */
+		public Node[] getChildren() {
+			return children;
+		}
 	}
 
 }
