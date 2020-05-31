b0cf63bc6435c36ad4fe852fe6b95d8d82968291
Kevin Liang <kev.liang@utexas.edu>
2020-05-30 21:15:41 -0500

    Day 8 part 1 completed

--- Day-8/Day_8.java
+++ Day-8/Day_8.java
@@ -1,42 +1,71 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Day_8 {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		/* part 1: ---------------------------------------------------------- */
-		Scanner fileIn = new Scanner(new File("Day-8/inputSmall.txt"));
+		Scanner fileIn = new Scanner(new File("Day-8/input.txt"));		
 		
-		// populate list of nodes
-		List<Node> nodeList = new ArrayList<>();
-		List<Integer> dataList = new ArrayList<>();
-		// process input
+		// process input file into LinkedList for O(1) removal
+		LinkedList<Integer> dataList = new LinkedList<>();
 		while (fileIn.hasNextInt()) {
 			// determine header params
 			dataList.add(fileIn.nextInt());
 		}
+		
+		// populate list of nodes with recursive helper
+		List<Node> nodeList = new ArrayList<>();
+		helper(nodeList, dataList);
+		
+		/* part 1:----------------------------------------------------------- */
+		
+		int totalMetadata = 0;
+		for (Node node : nodeList) {
+			totalMetadata += node.getMetadataSum();
+		}
+		System.out.printf("Part 1 solution: %d\n", totalMetadata);
+		
+		/* part 2:----------------------------------------------------------- */
+		
+//		System.out.printf("Part 2 solution: %d\n", totalMetadata);
 	}
 	
 	/*
 	 * Recursive helper to build list of nodes
 	 */
-	public static void helper(List<Node> list, List<Integer> data) {
-		
+	public static void helper(List<Node> list, LinkedList<Integer> data) {
+		if (!data.isEmpty()) {
+			// determine number of children and metadata
+			int numChildren = data.removeFirst();
+			int numEntries = data.removeFirst();
+			// call recursive helper on children
+			for (int i = 0; i < numChildren; i++) {
+				helper(list, data);
+			}
+			// determine metadata entries
+			int[] metadata = new int[numEntries];
+			for (int i = 0; i < numEntries; i++) {
+				metadata[i] = data.removeFirst();
+			}	
+			list.add(new Node(numChildren, metadata));
+		}
 	}
 	
 	/*
 	 * Node class for the tree data structure
 	 */
-	class Node {
+	static class Node {
 		
 		/*
 		 * header values
 		 */
 		private int numChildren;
 		private int numEntries;
+		
 		/*
 		 * metadata entries
 		 */
@@ -46,6 +75,15 @@ public class Day_8 {
 			this.numChildren = numChildren;
 			this.numEntries = metadata.length;
 			this.metadata = metadata;
+		}
+		
+		// calculate and return the sum of the node's metadata, for part 1
+		public int getMetadataSum() {
+			int total = 0;
+			for (int value : metadata) {
+				total += value;
+			}
+			return total;
 		}
 		
 	}
