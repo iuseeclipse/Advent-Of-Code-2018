5a2c92b92d99ba568c797bfc6beb4db8c72aeb31
Kevin Liang <kev.liang@utexas.edu>
2020-06-01 21:11:28 -0500

    Finished Day 10, Part 1 and 2

--- Day-10/Day_10.java
+++ Day-10/Day_10.java
@@ -1,11 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Day_10 {
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner fileIn = new Scanner(new File("Day-9/input.txt"));
+	public static void main(String[] args) throws IOException {
+		Scanner fileIn = new Scanner(new File("Day-10/input.txt"));
+		PointSimulator ps = new PointSimulator();
+		while (fileIn.hasNextLine()) {
+			String input = fileIn.nextLine();
+			int x = Integer.parseInt(input.substring(10, 16).trim());
+			int y = Integer.parseInt(input.substring(17, 24).trim());
+			int dX = Integer.parseInt(input.substring(36, 38).trim());
+			int dY = Integer.parseInt(input.substring(40, 42).trim());
+			ps.addPoint(x, y, dX, dY);
+		}
+		
+		/* Part 1: ---------------------------------------------------------- */
+		System.out.printf("Part 1 Solution:\n");
+		ps.run();
+		
+		/* Part 2: ---------------------------------------------------------- */
+		System.out.printf("Part 2 Solution: %d\n", ps.getTime());
 	}
 
 }
--- Day-10/PointSimulator.java
+++ Day-10/PointSimulator.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,10 +14,13 @@ import java.util.List;
 public class PointSimulator {
 	
 	/*
-	 * Point with (x, y) coordinates and velocity
+	 * nested class Point with (x, y) coordinates and velocity
 	 */
 	private class Point {
 		
+		/*
+		 * private instance variables to be accessed by PointSimulator class
+		 */
 		private int x;
 		private int y;
 		private int dX;
@@ -27,12 +33,33 @@ public class PointSimulator {
 			this.dY = dY;			
 		}
 		
+		/*
+		 * step function to "advance" one second, updating the position of the
+		 * Point based on its velocity
+		 */
+		public void step() {
+			x += dX;
+			y += dY;
+		}
+		
+		/*
+		 * undoes the step function to return one second
+		 */
+		public void unstep() {
+			x -= dX;
+			y -= dY;
+		}
 	}
 	
 	/*
 	 * internal container of all added Points
 	 */
 	private List<Point> points;
+
+	/*
+	 * track the seconds elapsed, or the number of steps called for each
+	 */
+	private int secondsElapsed;
 	
 	public PointSimulator() {
 		// implement with LinkedList rather than ArrayList to eliminate resizing
@@ -40,16 +67,123 @@ public class PointSimulator {
 	}
 	
 	/*
-	 * Add a point to the PointSimulator
+	 * Add a Point with given properties to the PointSimulator
 	 */
 	public void addPoint(int x, int y, int dX, int dY) {
 		points.add(new Point(x, y, dX, dY));
 	}
 	
 	/*
-	 * Run the simulation to determine the message
+	 * Run the simulation, stopping when the message solution is found.
 	 * precondition: all points added to PointSimulator
 	 */
-	
+	public void run() {
+		
+		// keep track of smallest bound for convergence
+		int diffMinX = Integer.MAX_VALUE;
+		int diffMinY = Integer.MAX_VALUE;
+		
+		boolean converges = true;
+		// points should converge until message is reached, then diverge
+		while (converges) {
+			// iterator for LinkedList, constant time
+			
+			// manage current outer bounds of all points in current step
+			int boundMinX = Integer.MAX_VALUE;
+			int boundMinY = Integer.MAX_VALUE;
+			int boundMaxX = Integer.MIN_VALUE;
+			int boundMaxY = Integer.MIN_VALUE;
+			for (Point point : points) {
+				
+				int currX = point.x;
+				int currY = point.y;
+//				System.out.printf("%d x, %d y\n", currX, currY);
+				// update bounds
+				if (currX > boundMaxX) {
+					boundMaxX = currX;
+				}
+				if (currX < boundMinX) {
+					boundMinX = currX;
+				}
+				if (currY > boundMaxY) {
+					boundMaxY = currY;
+				}
+				if (currY < boundMinY) {
+					boundMinY = currY;
+				}
+				point.step();
+			}
 
+			int currDiffX = boundMaxX - boundMinX;
+			int currDiffY = boundMaxY - boundMinY;
+			
+			// was there divergence past the step?
+			if ((diffMinX <= currDiffX) || diffMinY <= currDiffY &&
+					secondsElapsed > 0) {
+				converges = false;
+			}
+			// still converges, update the minimums
+			else {
+				if (diffMinX > currDiffX) {
+					diffMinX = currDiffX;
+				}
+				if (diffMinY > currDiffY) {
+					diffMinY = currDiffY;
+				}
+				secondsElapsed++;
+			}
+		}
+		
+		// divergence detected at current location of steps
+		// all points are two steps ahead, call unstep function to fix
+		secondsElapsed -= 2;
+		int startX = Integer.MAX_VALUE;
+		int startY = Integer.MAX_VALUE;
+		int endX = Integer.MIN_VALUE;
+		int endY = Integer.MIN_VALUE;
+		for (Point point : points) {
+			point.unstep();
+			point.unstep();
+			if (point.x < startX) {
+				startX = point.x;
+			}
+			if (point.y < startY) {
+				startY = point.y;
+			}
+			if (point.x > endX) {
+				endX = point.x;
+			}
+			if (point.y > endY) {
+				endY = point.y;
+			}			
+		}
+		// print the message from the diverged bounds, which should contain it
+		printMessage(startX, startY, endX, endY);
+	}
+	
+	/*
+	 * Return seconds elapsed for part 2, after run() is called
+	 */
+	public int getTime() {
+		return secondsElapsed;
+	}
+	
+	/*
+	 * Prints a visual representation of all Points in coordinate bounds, once 
+	 * run() has been called. 
+	 */
+	private void printMessage(int startX, int startY, int endX, int endY) {
+		for (int y = startY; y <= endY; y++) {
+			for (int x = startX; x <= endX; x++) {
+				String toPrint = ".";
+				for (Point point : points) {
+					if (point.x == x && point.y == y) {
+						toPrint = "#";
+					}
+				}
+				System.out.print(toPrint);
+			}
+			System.out.println("");
+		}
+	}
 }
